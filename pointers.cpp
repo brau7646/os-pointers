c494c8173e8e46bd8a791e7cdda56961797cf100
Andy Braunschweig <brau7646@stthomas.edu>
2021-02-10 17:50:02 -0600

    almost finished

--- pointers.cpp
+++ pointers.cpp
@@ -18,12 +18,39 @@ int main(int argc, char **argv)
 {
     Student student;
     double average;
+    
 
-    // Sequence of user input -> store in fields of `student`
+    student.id = promptInt("Please enter the student's id number: ", 0, 999999999);
+
+    student.f_name = new char[128];
+    student.l_name = new char[128];
+
+    std::cout << "Please enter the student's first name: ";
+    std::cin >> student.f_name;
+
+    std::cout << "Please enter the student's last name: ";
+    std::cin >> student.l_name;
+
+
+    student.n_assignments = promptInt("Please enter how many assignments were graded: ", 0, 134217728);
+    std::cout << "\n";
+    int i;
+    for (i=0; i < student.n_assignments; i++)
+    {
+        std::string assignmentGrade = "Please enter grade for assignment " + std::to_string(i) + ": ";
+        student.grades[i] = promptDouble(assignmentGrade,0.0,100.0);
+    }
+
+    std::cout <<"\nStudent: " << student.f_name << " " << student.l_name << " [" << std::to_string(student.id) << "]\n";
+
 
     // Call `CalculateStudentAverage(???, ???)`
     // Output `average`
-
+    calculateStudentAverage(&student,&average);
+    float floatAverage = (float) average;
+    std::cout <<"  Average grade: ";
+    printf("%.*f", 1,floatAverage);
+    std::cout<<"\n";
     return 0;
 }
 
@@ -35,6 +62,23 @@ int main(int argc, char **argv)
 int promptInt(std::string message, int min, int max)
 {
     // Code to prompt user for an int
+    int toReturn;
+    while (true)
+    {
+        std::cout << message;
+        std::cin >> toReturn;
+        if (std::cin.fail())
+        {
+            std::cin.clear();
+            std::cin.ignore(max,'\n');
+            std::cout << "Sorry, I cannot understand your answer"<<std::endl;
+        }
+        else
+        {
+            break;
+        }
+    }
+    return toReturn;
 }
 
 /*
@@ -44,7 +88,24 @@ int promptInt(std::string message, int min, int max)
 */
 double promptDouble(std::string message, double min, double max)
 {
-    // Code to prompt user for a double
+    // Code to prompt user for a double;
+    double toReturn;
+    while (true)
+    {
+        std::cout << message;
+        std::cin >> toReturn;
+        if (std::cin.fail())
+        {
+            std::cin.clear();
+            std::cin.ignore(max,'\n');
+            std::cout << "Sorry, I cannot understand your answer"<<std::endl;
+        }
+        else
+        {
+            break;
+        }
+    }
+    return toReturn;
 }
 
 /*
@@ -54,4 +115,14 @@ double promptDouble(std::string message, double min, double max)
 void calculateStudentAverage(void *object, double *avg)
 {
     // Code to calculate and store average grade
+    int i;
+    double sum=0.0;
+    Student student = *(Student *)object;
+    for (i=0; i < student.n_assignments; i++)
+    {
+        sum = sum + student.grades[i];
+    }
+    
+    *avg = sum/student.n_assignments;
+    
 }
